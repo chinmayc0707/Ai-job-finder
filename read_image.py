32a7ad495c4f0556fbc9a508672f8988352ec5ef
Chinmay C Bhat <139462368+chinmayc0707@users.noreply.github.com>
2026-06-22 11:48:19 +0530

    removed temp files

--- read_image.py
+++
@@ -1,5 +0,0 @@
-import base64
-
-with open("/home/jules/verification/chatbot-ui.png", "rb") as image_file:
-    encoded_string = base64.b64encode(image_file.read()).decode('utf-8')
-    print(encoded_string[:100] + "...(truncated)")
